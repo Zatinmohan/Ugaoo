3088ce394ee22e8a1a2491aa41d3f0f7c2c46d2d
Zatin <jatinmohan06@gmail.com>
2025-09-12 02:50:38 +0530

    - testing

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ugaoo',
+      title: 'Ugaoo testing',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
