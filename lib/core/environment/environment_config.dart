2e030d76595a6017129985d2760e0abcf94c5ce7
Zatin <jatinmohan06@gmail.com>
2025-09-13 02:52:25 +0530

    Update analysis options and dependencies; replace flutter_lints with very_good_analysis and adjust Dart SDK version

--- lib/core/environment/environment_config.dart
+++ lib/core/environment/environment_config.dart
@@ -1,7 +1,15 @@
+/// [EnvironmentConfig] is an abstract class that represents
+/// the environment configuration
 abstract class EnvironmentConfig {}
 
+/// [DevEnvironmentConfig] is a class that represents
+/// the development environment configuration
 class DevEnvironmentConfig implements EnvironmentConfig {}
 
+/// [ProdEnvironmentConfig] is a class that represents
+/// the production environment configuration
 class ProdEnvironmentConfig implements EnvironmentConfig {}
 
+/// [StgEnvironmentConfig] is a class that represents
+/// the staging environment configuration
 class StgEnvironmentConfig implements EnvironmentConfig {}
--- lib/core/environment/environment_config_manager.dart
+++ lib/core/environment/environment_config_manager.dart
@@ -1,13 +1,18 @@
 import 'package:ugaoo/core/environment/environment_config.dart';
 import 'package:ugaoo/core/environment/environment_type.dart';
 
+/// [EnvironmentConfigManager] is a class that manages
+/// the environment configuration
 class EnvironmentConfigManager {
+  /// This requires the [EnvironmentType] to be passed in the constructor
+  const EnvironmentConfigManager(this._environmentConfig);
+
   final EnvironmentType _environmentConfig;
 
-  EnvironmentConfigManager(this._environmentConfig);
-
+  /// It returns the name of the environment based on the [EnvironmentType]
   String get environmentName => _environmentConfig.name;
 
+  /// It returns the environment configuration based on the [EnvironmentType]
   EnvironmentConfig get environmentConfig => switch (_environmentConfig) {
         EnvironmentType.dev => DevEnvironmentConfig(),
         EnvironmentType.prod => ProdEnvironmentConfig(),
