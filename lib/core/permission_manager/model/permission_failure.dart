a6fa7ed3a3446c26c7afd2d9a7515a208135bd74
Zatin <jatinmohan06@gmail.com>
2025-09-21 05:49:14 +0530

    Update dependencies: add permission_handler and its related packages for improved permission management across platforms.

---
+++ lib/core/permission_manager/model/permission_failure.dart
@@ -0,0 +1,12 @@
+import 'package:ugaoo/utilities/failure.dart';
+
+/// [PermissionFailure] is a class that represents the failure for the permission.
+class PermissionFailure extends Failure {
+  /// Constructor of [PermissionFailure] that requires:
+  /// - [message] is the message of the failure
+  /// - [title] is the title of the failure
+  const PermissionFailure({
+    required super.message,
+    required super.title,
+  });
+}
