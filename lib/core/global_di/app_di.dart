4cecec3f108cbaa47b2a8b7f94115dd5bbc98b9e
Zatin <jatinmohan06@gmail.com>
2025-09-22 01:45:55 +0530

    Update dependencies: add firebase_core and enhance Firebase integration with direct dependency declaration in pubspec.lock.

--- lib/core/global_di/app_di.dart
+++ lib/core/global_di/app_di.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get_it/get_it.dart';
 import 'package:ugaoo/core/cache/cache_handler.dart';
@@ -6,6 +7,8 @@ import 'package:ugaoo/core/environment/environment_type.dart';
 import 'package:ugaoo/core/logger/log.dart';
 import 'package:ugaoo/core/package_info/handlers/package_info_handler.dart';
 import 'package:ugaoo/core/package_info/package_info_manager.dart';
+import 'package:ugaoo/core/remote_config/handlers/firebase_remote_config_handler.dart';
+import 'package:ugaoo/core/remote_config/remote_config_manager.dart';
 import 'package:ugaoo/core/router/handler/go_router_handler.dart';
 import 'package:ugaoo/core/router/manager/router_manager.dart';
 import 'package:ugaoo/core/storage/handler/database_handler/realm_storage_handler.dart';
@@ -19,6 +22,7 @@ part 'app_flavor_di.dart';
 part 'app_route_dependencies.dart';
 part 'storage_di.dart';
 part 'package_info_di.dart';
+part 'firebase_service_di.dart';
 
 /// [sl] is the singleton instance of [GetIt]
 final GetIt sl = GetIt.instance;
@@ -32,6 +36,7 @@ class AppDependencyInjection {
     try {
       _registerAppFlavorDependencies(flavor: flavor);
       _registerRouterDependencies();
+      _registerFirebaseServiceDependencies();
       _registerStorageDependencies();
       _registerPackageInfoDependencies();
     } catch (error) {
---
+++ lib/core/remote_config/models/remote_config_failures.dart
@@ -0,0 +1,12 @@
+import 'package:ugaoo/utilities/failure.dart';
+
+/// [RemoteConfigFailure] is a class that represents the failure for the remote config.
+class RemoteConfigFailure extends Failure {
+  /// Constructor of [RemoteConfigFailure] that requires:
+  /// - [message] is the message of the failure
+  /// - [title] is the title of the failure
+  const RemoteConfigFailure({
+    required super.message,
+    required super.title,
+  });
+}
