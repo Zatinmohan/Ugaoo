b82ae67469625938f336659830d4c5680b092cd7
Zatin <jatinmohan06@gmail.com>
2025-09-21 06:12:22 +0530

    Update dependencies: add cross_file, dbus, file_picker, flutter_plugin_android_lifecycle, petitparser, and xml for improved functionality and compatibility.

---
+++ lib/core/file_picker/handlers/file_picker_handleable.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: one_member_abstracts
+
+import 'package:fpdart/fpdart.dart';
+import 'package:ugaoo/core/file_picker/models/file_picker_failure.dart';
+import 'package:ugaoo/core/file_picker/models/selected_file_model.dart';
+
+/// [FilePickerHandleable] is an abstract class that represents
+/// the file picker handleable interface
+abstract class FilePickerHandleable {
+  /// This method is used to pick files from the system.
+  /// It returns a [TaskEither] that contains a list of [SelectedFileModel]
+  /// or a [FilePickerFailure].
+  /// - [allowedExtensions] is the allowed extensions of the files
+  /// - [withData] is the flag to include the data of the files
+  /// - [compressionQuality] is the compression quality of the files
+  /// - [allowMultiple] is the flag to allow multiple files to be picked
+  /// It returns a [TaskEither] that contains a list of [SelectedFileModel]
+  /// or a [FilePickerFailure].
+  TaskEither<FilePickerFailure, List<SelectedFileModel>> pickFiles({
+    List<String>? allowedExtensions,
+    bool withData = false,
+    int compressionQuality = 0,
+    bool allowMultiple = true,
+  });
+}
