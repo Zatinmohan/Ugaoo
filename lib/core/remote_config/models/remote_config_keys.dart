94f747ea7d44fb3c5e99f39560b7137fc40e1a47
Zatin <jatinmohan06@gmail.com>
2025-09-22 03:08:37 +0530

    Refactor FirebaseRemoteConfigHandler to use key.name for consistency, update RemoteConfigKey enum to uppercase, and modify HomeScreen to read remote config values, enhancing integration with Firebase remote configuration.

--- lib/core/remote_config/models/remote_config_keys.dart
+++ lib/core/remote_config/models/remote_config_keys.dart
@@ -1,7 +1,7 @@
 /// [RemoteConfigKey] is a class that represents the remote configuration keys.
 enum RemoteConfigKey {
   /// The key string associated with the remote configuration value.
-  testKey('test');
+  testKey('TEST');
 
   /// Constructor of [RemoteConfigKey] that requires:
   /// - [key] is the key string associated with the remote configuration value.
