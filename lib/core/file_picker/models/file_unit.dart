86e0614ce0af18af13cbab6ec16980aedc4bbc73
Zatin <jatinmohan06@gmail.com>
2025-09-21 06:12:22 +0530

    Update dependencies: add cross_file, dbus, file_picker, flutter_plugin_android_lifecycle, petitparser, and xml for improved functionality and compatibility.

---
+++ lib/core/file_picker/models/file_unit.dart
@@ -0,0 +1,14 @@
+/// This is the enum for the file unit
+enum FileUnit {
+  /// This is the bytes unit
+  bytes,
+
+  /// This is the kb unit
+  kb,
+
+  /// This is the mb unit
+  mb,
+
+  /// This is the gb unit
+  gb,
+}
