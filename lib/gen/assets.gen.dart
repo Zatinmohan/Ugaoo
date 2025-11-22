870ea562f9b884fe875b03463bd2429566c39f49
Zatin <jatinmohan06@gmail.com>
2025-11-23 03:12:01 +0530

    feat: Add login screen background images for dark and light themes and update asset generation.

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -9,6 +9,8 @@
 // ignore_for_file: type=lint
 // ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
 
+import 'package:flutter/widgets.dart';
+
 class $AssetsDataGen {
   const $AssetsDataGen();
 
@@ -18,6 +20,16 @@ class $AssetsDataGen {
   /// Directory path: assets/data/translations
   $AssetsDataTranslationsGen get translations =>
       const $AssetsDataTranslationsGen();
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// Directory path: assets/images/dark
+  $AssetsImagesDarkGen get dark => const $AssetsImagesDarkGen();
+
+  /// Directory path: assets/images/light
+  $AssetsImagesLightGen get light => const $AssetsImagesLightGen();
 }
 
 class $AssetsDataJsonGen {
@@ -43,8 +55,126 @@ class $AssetsDataTranslationsGen {
   List<String> get values => [enUS];
 }
 
+class $AssetsImagesDarkGen {
+  const $AssetsImagesDarkGen();
+
+  /// File path: assets/images/dark/login_background.webp
+  AssetGenImage get loginBackground =>
+      const AssetGenImage('assets/images/dark/login_background.webp');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [loginBackground];
+}
+
+class $AssetsImagesLightGen {
+  const $AssetsImagesLightGen();
+
+  /// File path: assets/images/light/login_background.webp
+  AssetGenImage get loginBackground =>
+      const AssetGenImage('assets/images/light/login_background.webp');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [loginBackground];
+}
+
 class Assets {
   const Assets._();
 
   static const $AssetsDataGen data = $AssetsDataGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+    this.animation,
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+  final AssetGenImageAnimation? animation;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class AssetGenImageAnimation {
+  const AssetGenImageAnimation({
+    required this.isAnimation,
+    required this.duration,
+    required this.frames,
+  });
+
+  final bool isAnimation;
+  final Duration duration;
+  final int frames;
 }
